4db4c1cdf6276f74e7f8df1f43c598f98ab61f9b
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-18 18:21:25 +0000

    Update Week2Basic1.py

--- 210CT CourseWork/Week 2/Week2Basic1.py
+++ 210CT CourseWork/Week 2/Week2Basic1.py
@@ -1,11 +1,12 @@
 numberUser = input("Enter Parameter: ")
+checkDone = False
 sqaureparameterNum = int(numberUser)
 currentNum = sqaureparameterNum
 
-while True:
+while checkDone == False:
     if sqaureparameterNum >= (currentNum * currentNum):
         print("The highest perfect square is: " + str(currentNum * currentNum))
-        break;
+        checkDone = True
 
     else:
         currentNum = currentNum - 1

ab1f7e1ed881a3286d4ccb61edab545fe1efaf27
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-19 18:20:58 +0000

    Update 19/11/2016 18:20
    Added annotation to code

--- 210CT CourseWork/Week 1/W1Basic1.py
+++ 210CT CourseWork/Week 1/W1Basic1.py
@@ -7,11 +7,11 @@ print("Starting list was: " + str(numList))
 
 currentY = 0
 
-while currentY < listLeng:
-    x = random.randint(0,(listLeng - 1))
-    currentNum = numList[currentY]
-    swapNum = numList[x]
-    numList[currentY] = swapNum
+while currentY < listLeng:               #Following code runs while currentY is less than the length of the list
+    x = random.randint(0,(listLeng - 1)) #sets x, a temp value, to a random number between 0 and 1 less than the length of the list
+    currentNum = numList[currentY]       #sets currentNum to the same value as postion currentY in list numList
+    swapNum = numList[x]                 #sets swapNum to the same value as postion x in list numList
+    numList[currentY] = swapNum          #Following two lines swap numList[currentY] with numList[x]
     numList[x] = currentNum
     currentY = currentY + 1
 

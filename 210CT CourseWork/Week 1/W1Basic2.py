a387497f354d583adc3d31de1b783a0cf3a78e4d
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-19 19:00:17 +0000

    Update 19/11/2016
    Added annotation

--- 210CT CourseWork/Week 1/W1Basic2.py
+++ 210CT CourseWork/Week 1/W1Basic2.py
@@ -1,23 +1,27 @@
 userNum = input("Enter Number: ")
-if "." not in userNum:
+if "." not in userNum:                  #This if statement checks if the user has added decimnals to the number
     print("To Check " + userNum + ".0")
+    userNumStr = str(userNum) + ".0"    # and if not adds it to the end for a represtation to show the user what numbr has been checked
+    print(userNumStr)
 else:
     print("To Check " + userNum)
+    userNumStr = str(userNum)
 i = 0
-userNumStr = str(userNum)
-pointsUsed = 0
-endi = len(userNum)
+
+pointsUsed = 1
+endi = len(userNumStr)           #base case for i count
 userNumList = list(userNumStr)
 print(userNumList)
 
 while i < endi:
-    if userNumList[i] != "0" and userNumList[i] != "." :
-        lastNumPos = i
+    if userNumList[i] != "0" and userNumList[i] != "." : #if the value of position i of userNumList isn't 0 or . then it adds on to the value of i and carrys on the while loop
+        lastNumPos = i  #Sets value of last non 0 number location to the current i as this will be used later to calculate 
         i = i + 1
-    elif userNumList[i] == ".":
-        pointsUsed = 1
-        i = i + 1
+    #elif userNumList[i] == ".":
+        #pointsUsed = 1   #set number of points used to 1
+        #i = i + 1
     else:
         i = i + 1
 
-print("Number of trailing 0's is: " + str((endi - lastNumPos - pointsUsed)))
+print("Number of trailing 0's is: " + str((endi - 1 - lastNumPos - pointsUsed)))
+#endi (length of list) - lastNumPos (postion value of last non 0 number) - pointsUsed (points used in number) = number of repeating 0's at end of number

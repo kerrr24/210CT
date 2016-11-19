75129773ea713f394c5db44e1c52a1011e65a10b
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-19 19:12:34 +0000

    Update 19/11/2016 19:12

--- 210CT CourseWork/Week 3/210CTWeek3Basic2.py
+++ 210CT CourseWork/Week 3/210CTWeek3Basic2.py
@@ -3,19 +3,25 @@ numberTry = int(numCheck)
 
 
 def primeCheck(number, divider):
-    numRemainder = number%divider
-    print(numRemainder)
 
-    if divider > 1:
-
-        if numRemainder == 0:
-            print(numCheck + " is not a prime number")
-
-        else:
-            dividerNum = divider - 1
-            primeCheck(numberTry, dividerNum)
-
-    else:
+    if number == 1:
         print(numCheck + " is a prime number")
 
-primeCheck(numberTry,numberTry)
+    else:
+        numRemainder = number%divider
+        print(numRemainder)
+
+        if divider > 1:
+
+            if numRemainder == 0:
+                print(numCheck + " is not a prime number")
+
+
+            else:
+                dividerNum = divider - 1
+                primeCheck(numberTry, dividerNum)
+
+        else:
+            print(numCheck + " is a prime number")
+
+primeCheck(numberTry,numberTry - 1)

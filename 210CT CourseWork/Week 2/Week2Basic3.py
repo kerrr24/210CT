003905cd4c245efdbd326cb2e861877060ca24fd
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-20 19:37:59 +0000

    Update 20/11/2016 19:37

--- 210CT CourseWork/Week 2/Week2Basic3.py
+++ 210CT CourseWork/Week 2/Week2Basic3.py
@@ -1,91 +1,89 @@
 Matrix1 <- UserInput
 Matrix2 <- UserInput
+MatrixBase <- Matrix of all 0's
 
-MATRIXADDITION(Matrix1, Matrix2)
+MATRIXADDITION(Matrix1, Matrix2, done)
 
-currentX <- 0
-currentY <- 0
+    currentX <- 0
+    currentY <- 0
+    matrixXMax <- len(Matrix1[0]) - 1
+    matrixYMax <- len(Matrix1) - 1
+    addMatrixResult <- MatrixBase
 
-while True
+    while done = True
 
-    if currentX <= matrixXMax
-        addMatrixResult(currentX,currentY) <- Matrix1(currentX,currentY) - Matrix2(currentX,currentY)
-        currentX <- currentX + 1
+        if currentY <= matrixYMax
 
-    else
-        currentX <- 0
-        currentY <- currentY + 1
-        
-        if currentY > matrixYMax
-            break
+            if currentX <= matrixXMax
+                addMatrixResult[currentY,currentX] <- Matrix1[currentY,currentX] + Matrix2[currentY,currentX]
+                currentX <- currentX + 1
 
-print addMatrixResult
+            else
+                currentX <- 0
+                currentY <- currentY + 1
+        else
+            print addMatrixResult
+            return addMatrixResult  
 
 
 MATRIXSUBSTRACTION(Matrix1, Matrix2)
 
-currentX <- 0
-currentY <- 0
+    currentX <- 0
+    currentY <- 0
+    matrixXMax <- len(Matrix1[0]) - 1
+    matrixYMax <- len(Matrix1) - 1
+    subMatrixResult <- MatrixBase
 
-while True
+    while done = True
 
-    if currentX <= matrixXMax
-        subMatrixResult(currentX,currentY) <- Matrix1(currentX,currentY) + Matrix2(currentX,currentY)
-        currentX <- currentX + 1
+        if currentY <= matrixYMax
 
-    else
-        currentX <- 0
-        currentY <- currentY + 1
-        
-        if currentY > matrixYMax
-            break
+            if currentX <= matrixXMax
+                subMatrixResult[currentY,currentX] <- Matrix1[currentY,currentX] - Matrix2[currentY,currentX]
+                currentX <- currentX + 1
 
-print subMatrixResult
+            else
+                currentX <- 0
+                currentY <- currentY + 1
+        else
+            print subMatrixResult
+            return subMatrixResult 
 
 
 MATRXIMULTIPLCATION(Matrix1, Matrix2)
 
-multiMatrixResult <- filled matrix of all 0
-currentX <- 0
-yCount <- 0
-xCount <- 0
-currentY <- 0
+    multiMatrixResult <- filled matrix of all 0
+    currentX <- 0
+    yCount <- 0
+    xCount <- 0
+    currentY <- 0
+    matrixXMax <- len(Matrix1[0]) - 1
+    matrixYMax <- len(Matrix1) - 1
+    MultiMatrixResult <- MatrixBase
 
-while True
+    while done = False
 
-    if currentX <= matrixXMax
+        if currentY <= matrixYMax
 
-        for xCount <= matrixXMax
+            if currentX <= matrixXMax
 
-            multiMatrixResult(currentX,currentY) <- multiMatrixResult(currentX,currentY) + (  Matrix1(xCount,currentY) * Matrix2(currentX,yCount) ) 
-            xCount <- xCount + 1
-            yCount <- yCount + 1
+                while xCount <= matrixXMax
 
-        currentX <- currentX + 1
-        
-    else
-    
-        currentX <- 0
-        currentY <- currentY + 1
-        
-        if currentY > matrixYMax
-            break
-        
-print multiMatrixResult
+                    multiMatrixResult[currentY][currentX] <- multiMatrixResult[currentX][currentY] + ( Matrix1[currentY][xCount] * Matrix2[yCount][currentX] ) 
+                    xCount <- xCount + 1
+                    yCount <- yCount + 1
 
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+                currentX <- currentX + 1
+                xCount <- 0
+                yCount <- 0
+                
+            else
+            
+                currentX <- 0
+                currentY <- currentY + 1
+                MATRXIMULTIPLCATION(Matrix1, Matrix2,currentX,currentY)
+                
+        else
+            done <- True
+            print MultiMatrixResult
+            return multiMatrixResult

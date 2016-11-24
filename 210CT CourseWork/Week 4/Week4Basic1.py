9a37ef73280f8852564af77c33931e5587f17f61
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-24 10:18:56 +0000

    Update Week4Basic1.py

--- 210CT CourseWork/Week 4/Week4Basic1.py
+++ 210CT CourseWork/Week 4/Week4Basic1.py
@@ -1,19 +1,43 @@
-
 userList = input("Enter values with spaces between: ")
 userList = userList.split()
-findelem = input("Number to check for: ")
+#findelem = int(input("Number to check for: "))
+lowestPara = int(input("Lowest Parameter: "))
+highestPara = int(input("Highest Parameter: "))
+done = False
 
 
-def binarySearch(array, point):
-    def repeat(first, last):
-        midPoint = (len(array)) / 2
-        if point == array[midPoint]:
-            return midPoint
-        elif point > midPoint:
-            return repeat(midPoint + 1, last)
-        elif point < midPoint:
-            return repeat(first, last - 1)
+def binarySearch(array, findelem, highestPara, isdone):
 
-    return repeat(0, len(userList)-1)
+    lowest = 0
+    sequenceList = userList
+    highest = len(userList)
 
-binarySearch(userList, findelem)
+    if isdone == False and findelem != highestPara:
+
+        print(findelem)
+
+        if findelem <= highestPara:
+
+            while len(sequenceList) > 0:
+
+                midPoint = (lowest + highest) // 2
+                if str(findelem) > array[midPoint]:
+                    lowest = midPoint + 1
+                    sequenceList = sequenceList[lowest:highest]
+                elif str(findelem) < array[midPoint]:
+                    highest = midPoint - 1
+                    sequenceList = sequenceList[lowest:highest]
+                else:
+                    print(" A value is in the array between " + str(lowestPara) + " and " + str(highestPara))
+                    global done
+                    done = True
+                    break;
+
+
+                binarySearch(userList, findelem + 1, highestPara, done)
+
+
+    else:
+        pass;
+            
+binarySearch(userList, lowestPara, highestPara, False)

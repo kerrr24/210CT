c0c54cf781e5029e0c500dc540d56c9cbaf8f9d7
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-26 15:51:40 +0000

    Update 26/11/2016 15:51

--- 210CT CourseWork/Week 4/Week4Basic1.py
+++ 210CT CourseWork/Week 4/Week4Basic1.py
@@ -1,43 +1,35 @@
 userList = input("Enter values with spaces between: ")
 userList = userList.split()
-#findelem = int(input("Number to check for: "))
 lowestPara = int(input("Lowest Parameter: "))
 highestPara = int(input("Highest Parameter: "))
-done = False
 
 
-def binarySearch(array, findelem, highestPara, isdone):
+def binarySearch(array):
 
     lowest = 0
-    sequenceList = userList
-    highest = len(userList)
-
-    if isdone == False and findelem != highestPara:
-
-        print(findelem)
-
-        if findelem <= highestPara:
-
-            while len(sequenceList) > 0:
-
-                midPoint = (lowest + highest) // 2
-                if str(findelem) > array[midPoint]:
-                    lowest = midPoint + 1
-                    sequenceList = sequenceList[lowest:highest]
-                elif str(findelem) < array[midPoint]:
-                    highest = midPoint - 1
-                    sequenceList = sequenceList[lowest:highest]
-                else:
-                    print(" A value is in the array between " + str(lowestPara) + " and " + str(highestPara))
-                    global done
-                    done = True
-                    break;
+    highest = len(userList) - 1
+    midPointStopper = -1
 
 
-                binarySearch(userList, findelem + 1, highestPara, done)
+    while True:
 
-
-    else:
-        pass;
+            midPoint = (lowest + highest) // 2
             
-binarySearch(userList, lowestPara, highestPara, False)
+            if int(array[midPoint]) < lowestPara and midPointStopper != midPoint:
+                lowest = midPoint + 1
+                midPointStopper = midPoint
+                
+            elif int(array[midPoint]) > highestPara and midPointStopper != midPoint:
+                highest = midPoint - 1
+                mipPointStopper = midPoint
+                
+            elif str(highestPara) > array[midPoint] and str(lowestPara) < array[midPoint]:
+                 print(" A value is in the array between " + str(lowestPara) + " and " + str(highestPara))
+                 break;
+
+            else:     
+                print("No Valuse Present Between Given Parameters")
+                break;
+        
+            
+binarySearch(userList)

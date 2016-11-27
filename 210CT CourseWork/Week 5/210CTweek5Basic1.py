323f15bd234ee4c49758b17a602c748c03b3de83
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-27 18:18:38 +0000

    Update 27/11/2016 18:18
    added comments

--- 210CT CourseWork/Week 5/210CTweek5Basic1.py
+++ 210CT CourseWork/Week 5/210CTweek5Basic1.py
@@ -1,7 +1,9 @@
 numlist = str(input("List of Numbers (With space between them): "))
-numlistsplit = numlist.split(" ")
+numlistsplit = numlist.split(" ") #splits numbers on spaces also creates list
 numlistsplit.append("")
 listLen = len(numlistsplit)
+oneormeore = False
+
 
 def longSub(i, longestList):
 
@@ -10,23 +12,23 @@ def longSub(i, longestList):
 
     while i < listLen:
 
-        if numlistsplit[i] > numlistsplit[i-1]:
-            lengList.append(numlistsplit[i])
+        if numlistsplit[i] > numlistsplit[i-1]: #if the current element is greater than the previous element in the list then the followigng code runs
+            lengList.append(numlistsplit[i]) #adds number at postion i to list of current length check
             i = i + 1
             
-        elif numlistsplit[i] < numlistsplit[i-1]:
+        elif numlistsplit[i] < numlistsplit[i-1]: #if current element less than previous element then following code runs
             
-            if len(lengList) > len(longestList):
+            if len(lengList) > len(longestList): #if the length of the current list is greater than the longestpossible then the following code runs
                 
                 i = i + 1
-                longestList = lengList
-                longSub(i, lengList)
+                longestList = lengList #sets longestlist to the current list being made
+                longSub(i, lengList)# recursive call with updated values
   
             else:
-                break;
+                break; #stops loop
             
         else:
             i = i + 1
-            longSub(i, longestList)
+            longSub(i, longestList) #recursive call with updated values
             
 longSub(1, [])

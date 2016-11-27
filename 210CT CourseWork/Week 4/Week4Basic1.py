09845c2d7d8b7a8c7b3778335e8c2eeb534d5106
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-27 18:12:46 +0000

    Update 27/11/2016 18:12
    added comments

--- 210CT CourseWork/Week 4/Week4Basic1.py
+++ 210CT CourseWork/Week 4/Week4Basic1.py
@@ -8,28 +8,28 @@ def binarySearch(array):
 
     lowest = 0
     highest = len(userList) - 1
-    midPointStopper = -1
+    midPointStopper = -1 #used as a value to stop the loop, stops if statements from running
 
 
-    while True:
+    while True: #loop runs forever until a break occurs
 
-            midPoint = (lowest + highest) // 2
+            midPoint = (lowest + highest) // 2 #finds the mid point
             
             if int(array[midPoint]) < lowestPara and midPointStopper != midPoint:
                 lowest = midPoint + 1
-                midPointStopper = midPoint
+                midPointStopper = midPoint #sets the stopper to the value of the midPoint so that if the same midPoiint is used again then the if statment won't run as it implies that no element from the list is between the parameters as all midPoints have been used
                 
             elif int(array[midPoint]) > highestPara and midPointStopper != midPoint:
                 highest = midPoint - 1
-                mipPointStopper = midPoint
+                mipPointStopper = midPoint #sets the stopper to the value of the midPoint so that if the same midPoiint is used again then the if statment won't run as it implies that no element from the list is between the parameters as all midPoints have been used
                 
-            elif str(highestPara) > array[midPoint] and str(lowestPara) < array[midPoint]:
+            elif str(highestPara) > array[midPoint] and str(lowestPara) < array[midPoint]: #checks to see if midPoint is between both parameter set by the user
                  print(" A value is in the array between " + str(lowestPara) + " and " + str(highestPara))
-                 break;
+                 break; #stops the loop
 
             else:     
                 print("No Valuse Present Between Given Parameters")
-                break;
+                break; #stops the loop
         
             
 binarySearch(userList)

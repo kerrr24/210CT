3619e067ba80982be0fe9a5e6962f0a01eae6a66
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-27 18:04:54 +0000

    Update 27/11/2016 18:04
    added comments

--- 210CT CourseWork/Week 3/210CTWeek3Basic3.py
+++ 210CT CourseWork/Week 3/210CTWeek3Basic3.py
@@ -1,28 +1,26 @@
 wordVowel = input("Enter Word: ")
-wordVowelList = list(wordVowel)
+wordVowelList = list(wordVowel) #creates list using the word the user entered
 wordVowelList.append("")
 endlen = len(wordVowelList) - 1
 count = 0
 
-
 def vowelRemover(count, letter):
     if count < endlen:
-        if wordVowelList[count] in "aeiouAEIOU":
+        if wordVowelList[count] in "aeiouAEIOU": #checks if selected letter is in the string aeiouAEIOU, meaing any vowels will be removed 
             wordVowelList.remove(wordVowelList[count])
             global endlen
             endlen = endlen - 1
-            vowelRemover(count, wordVowelList[count])
+            vowelRemover(count, wordVowelList[count]) #recurive call with updated values
             
-        elif wordVowelList[count] not in "aeiouAEIOU":
+        elif wordVowelList[count] not in "aeiouAEIOU": #checks if selected letter is not in the string aeiouAEIOU
             count = count + 1
-            vowelRemover(count, wordVowelList[count])
+            vowelRemover(count, wordVowelList[count]) #recurive call with updated values
             
         else:
             pass
             
     else:
-        vowellessWord = "".join(wordVowelList)
+        vowellessWord = "".join(wordVowelList) #combines all the letters in the list without any space
         print(vowellessWord)
 
 vowelRemover(count, wordVowelList[count])
-

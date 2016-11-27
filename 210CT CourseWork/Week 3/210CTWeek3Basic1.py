5f88a98fff3801327769a96bd4fe51d2f1f7e527
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-11-27 17:46:47 +0000

    Update 27/11/2016 17:46
    added comments

--- 210CT CourseWork/Week 3/210CTWeek3Basic1.py
+++ 210CT CourseWork/Week 3/210CTWeek3Basic1.py
@@ -1,14 +1,14 @@
 origSentence = str(input("Sentence: "))
-wordList = origSentence.split()
+wordList = origSentence.split() #splits sentence at a space and creates a list using them
 print(wordList)
 listLen = len(wordList)
 i = 0
 
 while i < (listLen/2):
-    temp = wordList[i]
-    wordList[i] = wordList[listLen-(i + 1)]
-    wordList[listLen-(i + 1)] = temp
+    temp = wordList[i] #gets the value of the word from location i
+    wordList[i] = wordList[listLen-(i + 1)] #changes word in location i to word from location from word from location (i away from end) 
+    wordList[listLen-(i + 1)] = temp #sets word in (i away from end) to the temp value create two lines up
     i = i + 1
 
-revWordList = " ".join(wordList)
+revWordList = " ".join(wordList) #combines words in list by adding spaces between them
 print(revWordList)

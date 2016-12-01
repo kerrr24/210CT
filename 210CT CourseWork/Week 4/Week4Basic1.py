0f56859e7d0afbaa35a827e6c801098dc680389e
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-12-01 18:42:22 +0000

    Update 01/12/2016 18:42
    Correct int string comparison error

--- 210CT CourseWork/Week 4/Week4Basic1.py
+++ 210CT CourseWork/Week 4/Week4Basic1.py
@@ -1,4 +1,4 @@
-userList = input("Enter values with spaces between: ")
+erList = input("Enter values with spaces between: ")
 userList = userList.split()
 lowestPara = int(input("Lowest Parameter: "))
 highestPara = int(input("Highest Parameter: "))
@@ -14,6 +14,7 @@ def binarySearch(array):
     while True: #loop runs forever until a break occurs
 
             midPoint = (lowest + highest) // 2 #finds the mid point
+            print(midPoint)
             
             if int(array[midPoint]) < lowestPara and midPointStopper != midPoint:
                 lowest = midPoint + 1
@@ -23,13 +24,15 @@ def binarySearch(array):
                 highest = midPoint - 1
                 mipPointStopper = midPoint #sets the stopper to the value of the midPoint so that if the same midPoiint is used again then the if statment won't run as it implies that no element from the list is between the parameters as all midPoints have been used
                 
-            elif str(highestPara) > array[midPoint] and str(lowestPara) < array[midPoint]: #checks to see if midPoint is between both parameter set by the user
+            elif int(highestPara) > int(array[midPoint]) and int(lowestPara) < int(array[midPoint]): #checks to see if midPoint is between both parameter set by the user
                  print(" A value is in the array between " + str(lowestPara) + " and " + str(highestPara))
                  break; #stops the loop
 
             else:     
                 print("No Valuse Present Between Given Parameters")
                 break; #stops the loop
+
+                
         
             
 binarySearch(userList)

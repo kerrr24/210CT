3919830b1a7d1c12b5456463c942c678a8dacd3c
kerrr24 <kerrr3@uni.coventry.ac.uk>
2016-12-02 19:29:04 +0000

    update 02/12/2016 19:28
    removed one 0 at end error

--- 210CT CourseWork/Week 1/W1Basic2.py
+++ 210CT CourseWork/Week 1/W1Basic2.py
@@ -5,6 +5,7 @@ if "." not in userNum:                  #This if statement checks if the user ha
     print(userNumStr)
 else:
     print("To Check " + userNum)
+
     userNumStr = str(userNum)
 i = 0
 
@@ -17,11 +18,15 @@ while i < endi:
     if userNumList[i] != "0" and userNumList[i] != "." : #if the value of position i of userNumList isn't 0 or . then it adds on to the value of i and carrys on the while loop
         lastNumPos = i  #Sets value of last non 0 number location to the current i as this will be used later to calculate 
         i = i + 1
-    #elif userNumList[i] == ".":
-        #pointsUsed = 1   #set number of points used to 1
-        #i = i + 1
+    elif userNumList[i] == ".":
+        pointsUsed = 1   #set number of points used to 1
+        i = i + 1
     else:
         i = i + 1
+listOfchecknums = ['1','2','3','4','5','6','7','8','9']
+if userNumList[i- 2] in listOfchecknums  and userNumList[i-1] == '0': : #check to see if only one 0 at end
+    print("Number of trailing 0's is: 1")
 
-print("Number of trailing 0's is: " + str((endi - 1 - lastNumPos - pointsUsed)))
-#endi (length of list) - lastNumPos (postion value of last non 0 number) - pointsUsed (points used in number) = number of repeating 0's at end of number
+else:
+    print("Number of trailing 0's is: " + str((endi - 1 - lastNumPos - pointsUsed)))
+    #endi (length of list) - lastNumPos (postion value of last non 0 number) - pointsUsed (points used in number) = number of repeating 0's at end of number
